e286c5a146f68d6ea472a40e421fe30cc7dff816
khalil ladjal <khalilladjal@gmail.com>
2021-10-29 23:55:38 +0100

    2nd try cleaning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { UserSession, AppConfig } from "blockstack";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const appConfig = new AppConfig(["store_write"]);
+
+const userSession = new UserSession({ appConfig: appConfig });
+
+
+
+
+
+class App extends React.Component { 
+
+  handleSignIn = () => {
+
+    userSession.redirectToSignIn()
+    
+    }
+    
+render() {
+
+return <div>Nice Meme
+  <button className="ui button positive" onClick={this.handleSignIn}>
+
+Sign in 
+
+</button>
+</div>
+
+
+
 }
 
-export default App;
+}
+
+
+export default App
